aff8ad17710b97345a691bfbe8e7c31cb201c75d
DmitriyKor <dk@das-ua.com>
2025-05-05 20:58:54 +0300

    feat: 32 started

---
+++ homework32/main.js
@@ -0,0 +1,25 @@
+const
+    API_URL = 'http://www.omdbapi.com';
+    API_KEY = 'fda20e87';
+  
+function getURL(title) {
+    return `${API_URL}/?s=${title}&page=${page}&apikey=${API_KEY}`;
+}
+
+async function getData(title) {
+    try {
+      const url = getURL(title);
+      console.log(url);
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      console.log('getData:');
+      console.log(json);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  getData('pulp');
