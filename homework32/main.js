66745039ec8f60e66df81369549be8b9e68dba25
DmitriyKor <dmytro.kor75@gmail.com>
2025-05-08 14:24:56 +0300

    feat: done

--- homework32/main.js
+++ homework32/main.js
@@ -1,24 +1,42 @@
 const
     API_URL = 'http://www.omdbapi.com';
     API_KEY = 'fda20e87';
+    RESULTS_PER_PAGE = 10;
   
 function prepareURL(search_str, page){
     return encodeURI(`${API_URL}/?s=${search_str}&page=${page}&apikey=${API_KEY}`);
 }
 
 async function searchData(search_str, page) {
-    try {
+  indicator.className='indicator indicator_visible';
+  movies.textContent = '';  
+  try {
       const url = prepareURL(search_str, page);
-      console.log(url);
       const response = await fetch(url);
       if (!response.ok) {
         throw new Error(`Response status: ${response.status}`);
       }
-      const jsonData = await response.json();
-      return jsonData;
+      const json = await response.json();
+      totalSearchResults=0;
+      dataAvailable = json["Response"].toLowerCase() == "true"; 
+      if (dataAvailable) {  
+        totalSearchResults = json.totalResults;    
+        
+        for (film of json["Search"]) {
+          //const response = await fetch(film.Poster, { method: 'HEAD' });
+          //if (response.ok) {         
+            addMovie(film.Title, film.Year, film.Poster);
+          //}
+        }
+      }
+      
     } catch (error) {
       console.log(error.message);
+    } finally {
+      indicator.className='indicator';
+      updatePagesInfo();
     }
+
   }
 
 function addMovie (name, year, image_url) {
@@ -40,38 +58,56 @@ function addMovie (name, year, image_url) {
 
   movies.append(movie_item);
 }
-  
-async function handleSearchInput(e){
-  indicator.className='indicator indicator_visible';
-  try {
-    movies.textContent = '';
-    const json = await searchData(search.value , 1);
-        
-    result_p.innerText = JSON.stringify(json);
-    dataAvailable = json["Response"].toLowerCase() == "true"; 
-    if (dataAvailable) {      
-      for (film of json["Search"]) {
-        //const response = await fetch(film.Poster, { method: 'HEAD' });
-        //if (response.ok) {         
-          addMovie(film.Title, film.Year, film.Poster);
-        //}
-      }
+
+function handleSearchInput(e){
+  currentPage = 1;  
+  searchData(search.value , currentPage);
+}
+
+function updatePagesInfo(){
+  console.log(`totalSearchResults: ${totalSearchResults}, currentPage: ${currentPage}`);
+  if (totalSearchResults>0) {
+    pages.className='pages pages_visible';
+    pages_info.innerText=`${(currentPage-1)*RESULTS_PER_PAGE+1}..${Math.min(currentPage*RESULTS_PER_PAGE, totalSearchResults)} of ${totalSearchResults}`;
+    if (currentPage==1) {
+      pages_left.className='material-symbols-outlined page_arrow page_arrow_disabled';
+      console.log('arrow disabled');
+    } else {
+      pages_left.className='material-symbols-outlined page_arrow';
     }
-  } finally {
-    indicator.className='indicator';
+    if (currentPage*RESULTS_PER_PAGE >= totalSearchResults) {
+      pages_right.className='material-symbols-outlined page_arrow page_arrow_disabled'
+    } else {
+      pages_right.className='material-symbols-outlined page_arrow';
+    }
+  } else {
+    pages.className='pages';
+  }
+}
+
+function handlePageClick(e) {
+  if (e.target.id=="pages_left") {
+    if (currentPage>1) {
+      currentPage--;
+      searchData(search.value , currentPage);
+    }
+  } else {
+    if (currentPage * RESULTS_PER_PAGE < totalSearchResults) {
+      currentPage++;
+      searchData(search.value , currentPage);
+    }
   }
 }
 
 document.getElementById('search')?.addEventListener("input", handleSearchInput);    
-result_p = document.getElementById('result');
 
-movies = document.querySelector(".movies");
-indicator = document.querySelector(".indicator");
-
-  // function handleSearchInput(e) { 
-  //   console.log('change event'); 
-  //   searchData(search.value, 1).then(json => { 
-  //     console.log(json); result_p.innerText = JSON.stringify(json); // Або виведіть json.whatever }); 
-  //     })
-  // }
-
+let movies = document.querySelector(".movies");
+let indicator = document.querySelector(".indicator");
+let pages = document.getElementById('pages');
+let pages_info = document.getElementById('pages_info');
+let pages_left = document.getElementById('pages_left');
+pages_left.addEventListener("click", handlePageClick);
+let pages_right = document.getElementById('pages_right');
+pages_right.addEventListener("click", handlePageClick);
+let currentPage = 1;
+let totalSearchResults = 0;
