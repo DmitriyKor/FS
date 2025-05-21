0aed150ce2c374819ce6059b804a5ea01e16e513
DmitriyKor <dk@das-ua.com>
2025-05-21 19:29:09 +0300

    feat: 35 continued

--- homework35/webpack.config.js
+++ homework35/webpack.config.js
@@ -3,14 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerWebpackPlugin = require('css-minimizer-webpack-plugin' )
+const TerserPlugin = require('terser-webpack-plugin' );
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const filename = (ext) => isDev ? `[name].${ext}` : `[name].[fullhash].${ext}`;
+
+const optimization = () =>
+    ({ 
+    splitChunks: {chunks: 'all'},
+    minimizer: [
+    new CssMinimizerWebpackPlugin (),
+    new TerserPlugin()
+    ]
+});
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
-    entry: './main.js',
+    entry: {
+        main: ['./main.js'],
+    },
     mode: 'development',
     target: 'web',
+    optimization: optimization (),
     output: {
-        filename: 'bundle.js',
+        filename: filename('js'),
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
@@ -23,7 +42,7 @@ module.exports = {
             }]
         }),
         new MiniCssExtractPlugin({
-            filename: '[name].css'
+            filename: './styles/'+filename('css')
         })
     ],
     module: {
@@ -36,8 +55,40 @@ module.exports = {
                 },
             },
             {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {loader: 'babel-loader',
+                options: {presets: [
+                '@babel/preset-env',
+                '@babel/preset-typescript'
+                ]}
+                }
+            }, 
+            {
+                test: / \.(ttf|woff|woff2|eot)$/,
+                type: 'asset/resource',
+            },
+            {
                 test: /\.css$/,
-                use: [{loader: MiniCssExtractPlugin.loader} ,'css-loader']
+                use: [{loader: MiniCssExtractPlugin.loader},'css-loader']  // 'style-loader' 
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [{loader: MiniCssExtractPlugin.loader, options: {publicPath: ''}},
+                'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,              
+                use: {
+                  loader: "babel-loader",
+                  options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: [
+                        '@babel/plugin-proposal-class-properties'
+                    ]
+                  }
+                }
             }
         ]
     },
