cf4354a68f15abcb164dbf3216d23ce69b1f410a
DmitriyKor <dmytro.kor75@gmail.com>
2025-07-14 16:41:32 +0300

    feat: login continued

--- homework45/src/pages/login/index.tsx
+++ homework45/src/pages/login/index.tsx
@@ -1,14 +1,28 @@
 import { Form, Field } from 'react-final-form';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
-import { FormControl, FormControlLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, Stack, TextField } from '@mui/material';
+import { Alert, FormControl, FormControlLabel, InputLabel, MenuItem, Radio, RadioGroup, Select, Stack, TextField } from '@mui/material';
 import { required } from '../../shared/validation';
 import { SideBar } from '../../shared/components/sideBar';
 import { LoginLayout, LoginFormStyle } from './index.styles';
+import type { AxiosResponse } from 'axios';
+import axios from 'axios';
+import { API_URL } from '../../store/const';
+import { useState } from 'react';
 
 export const Login = () => {
 
-    const onSubmit = () => {
+    const [loginError, setLoginError] = useState('');
+
+    const onSubmit = async (values:any) => {
+        try {
+            console.log(values);
+            const response  : AxiosResponse = await axios(API_URL+'/login', values);
+            console.log(response); 
+
+        } catch (e) {
+            setLoginError(e.message);
+        }
 
     }
 
@@ -49,6 +63,7 @@ export const Login = () => {
                             </Field>
                         </Stack>
                         <Button sx={{mt: 2}} variant="outlined" type='submit'>Login</Button>
+                        {loginError!="" && <Alert severity="error">{loginError}</Alert>}
                     </form>
                 )}
             />
